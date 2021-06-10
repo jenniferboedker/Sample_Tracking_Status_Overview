99790be6e6d849c3cdccafb6ed71777f157bd8a8
jnnfr <jennifer.boedker@qbic.uni-tuebingen.de>
2021-06-10 16:35:51 +0200

    Feature/use multi maven project structure (#3)
    Make sample-tracking-status-overview a multi-module maven project
    Co-authored-by: Steffengreiner <Steffen.Greiner@gmx.de>

---
+++ sample-tracking-status-overview-app/src/main/groovy/life/qbic/portal/sampletracking/StatusOverviewApp.groovy
@@ -0,0 +1,60 @@
+package life.qbic.portal.sampletracking
+
+import com.vaadin.annotations.Theme
+import com.vaadin.server.Page
+import com.vaadin.server.VaadinRequest
+import com.vaadin.ui.HorizontalLayout
+import com.vaadin.ui.Layout
+import com.vaadin.ui.Notification
+import com.vaadin.ui.VerticalLayout
+import groovy.transform.CompileStatic
+import groovy.util.logging.Log4j2
+import life.qbic.portal.sampletracking.components.StyledNotification
+
+
+/**
+ * Entry point for the application. This class derives from {@link life.qbic.portal.portlet.QBiCPortletUI}.
+ *
+ * @since: 0.1.0
+ * @author: Jennifer Bödker
+ * @see <a href=https://github.com/qbicsoftware/portal-utils-lib>portal-utils-lib</a>
+ */
+@Theme("mytheme")
+@SuppressWarnings("serial")
+@Log4j2
+@CompileStatic
+class StatusOverviewApp extends QBiCPortletUI {
+
+    StatusOverviewApp() {
+        super()
+        // The constructor MUST NOT fail since the user does not get any feedback otherwise.
+        try {
+        //todo create the app here
+        } catch (Exception e) {
+            log.error("Could not initialize {}", StatusOverviewApp.getCanonicalName(), e)
+        } catch (Error error) {
+            log.error("Unexpected runtime error.", error)
+        }
+    }
+
+    @Override
+    protected Layout getPortletContent(final VaadinRequest request) {
+        def layout
+        log.info "Generating content for class {}", StatusOverviewApp.getCanonicalName()
+        try {
+            //todo add the app layout here
+            layout = new HorizontalLayout() //this.dependencyManager.getPortletView()
+        } catch (Exception e) {
+            log.error("Failed generating content for class {}", StatusOverviewApp.getCanonicalName())
+            log.error(e)
+            String errorCaption = "Application not available"
+            String errorMessage = "We apologize for any inconveniences. Please inform us via email to support@qbic.zendesk.com."
+            StyledNotification initializationErrorNotification = new StyledNotification
+                    (errorCaption, errorMessage, Notification.Type.ERROR_MESSAGE)
+            initializationErrorNotification.show(Page.getCurrent())
+            layout = new VerticalLayout()
+        }
+        return layout
+    }
+
+}
