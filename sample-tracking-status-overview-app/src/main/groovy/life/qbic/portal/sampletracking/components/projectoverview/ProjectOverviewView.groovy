37e8407fe326ecc0b18b9feb81ab7e3802573c97
jenniferboedker <jennifer.boedker@qbic.uni-tuebingen.de>
2021-06-15 12:05:39 +0200

    Add view and viewmodel with description

---
+++ sample-tracking-status-overview-app/src/main/groovy/life/qbic/portal/sampletracking/components/projectoverview/ProjectOverviewView.groovy
@@ -0,0 +1,12 @@
+package life.qbic.portal.sampletracking.components.projectoverview
+/**
+ * <h1>This class generates the layout for the ProductOverview use case</h1>
+ *
+ * <p>This view will be the entry point for the user and provides an overview of her projects. And the projects overall status.
+ * From here, the user can navigate to the StatusOverview.</p>
+ *
+ * @since 1.0.0
+ *
+*/
+class ProjectOverviewView {
+}
---
+++ sample-tracking-status-overview-app/src/main/groovy/life/qbic/portal/sampletracking/components/projectoverview/ProjectOverviewViewModel.groovy
@@ -0,0 +1,11 @@
+package life.qbic.portal.sampletracking.components.projectoverview
+/**
+ * <h1>ViewModel for the {@link ProjectOverviewView}</h1>
+ *
+ * <p>This model stores the values that are displayed on the {@link ProjectOverviewView}</p>
+ *
+ * @since 1.0.0
+ *
+*/
+class ProjectOverviewViewModel {
+}
