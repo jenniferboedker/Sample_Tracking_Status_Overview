61eb046aa6980d8209dbe4d7f9860d744ff167e1
jnnfr <jennifer.boedker@qbic.uni-tuebingen.de>
2021-06-18 08:47:04 +0200

    Update GridUtils description
    Co-authored-by: Steffengreiner <Steffen.Greiner@gmx.de>

--- sample-tracking-status-overview-app/src/main/groovy/life/qbic/portal/sampletracking/components/GridUtils.groovy
+++ sample-tracking-status-overview-app/src/main/groovy/life/qbic/portal/sampletracking/components/GridUtils.groovy
@@ -1,13 +1,10 @@
 package life.qbic.portal.sampletracking.components
 
 /**
- * <h1><short description></h1>
+ * A helper class with static utility functions for Vaadin Grids.
  *
- * <p><detailed description></p>
- *
- * @since <versiontag>
- *
-*/
+ * @since 1.0.0
+ */
 class GridUtils {
     final static int DESCRIPTION_COLUMN_LIMIT = 400
 }
