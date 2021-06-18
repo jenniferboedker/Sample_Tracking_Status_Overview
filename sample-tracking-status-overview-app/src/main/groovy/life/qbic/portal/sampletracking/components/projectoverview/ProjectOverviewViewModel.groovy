a66dd0757b55a1e60c9cf79379851045b6448dd9
jnnfr <jennifer.boedker@qbic.uni-tuebingen.de>
2021-06-18 10:14:36 +0200

    Mock project overview grid (#14)
    Implement mock project overview grid
    Co-authored-by: Steffengreiner <Steffen.Greiner@gmx.de>

--- sample-tracking-status-overview-app/src/main/groovy/life/qbic/portal/sampletracking/components/projectoverview/ProjectOverviewViewModel.groovy
+++ sample-tracking-status-overview-app/src/main/groovy/life/qbic/portal/sampletracking/components/projectoverview/ProjectOverviewViewModel.groovy
@@ -1,4 +1,9 @@
 package life.qbic.portal.sampletracking.components.projectoverview
+
+import life.qbic.business.projectoverview.Project
+import life.qbic.datamodel.samples.Location
+import life.qbic.datamodel.samples.Sample
+
 /**
  * <h1>ViewModel for the {@link ProjectOverviewView}</h1>
  *
@@ -8,4 +13,24 @@ package life.qbic.portal.sampletracking.components.projectoverview
  *
 */
 class ProjectOverviewViewModel {
+
+    List<Project> projects = []
+
+    ProjectOverviewViewModel(){
+        generateMockData()
+    }
+
+    void generateMockData(){
+        Location location = new Location()
+
+        Sample sample = new Sample()
+        sample.setCode("Q1234AC")
+        sample.setCurrentLocation(location)
+
+        Project project1 = new Project.Builder("Q1234","This is a test project",[sample]).progress(0).build()
+        Project project2 = new Project.Builder("Q1244","This is a test project 2",[sample]).progress(1).build()
+        Project project3 = new Project.Builder("Q1233","This is a test project 3",[sample]).progress(0.9).failedSamples(1).build()
+
+        projects.addAll(project1,project2,project3)
+    }
 }
---
+++ sample-tracking-status-overview-domain/src/main/groovy/life/qbic/business/projectoverview/Project.groovy
@@ -0,0 +1,64 @@
+package life.qbic.business.projectoverview
+
+import life.qbic.datamodel.samples.Sample
+
+/**
+ * <h1>A QBiC project</h1>
+ *
+ * <p>A project contains a unique project code, a description and the samples registered for this project.
+ * Furthermore, a projects progress can be determined form the {@link FailedSamplesRatio}.</p>
+ *
+ * @since 1.0.0
+ *
+*/
+class Project {
+
+    final String projectCode
+    final String projectDescription
+    final List<Sample> samples
+    float projectProgress
+    FailedSamplesRatio failedSamples
+    Date lastUpdate
+
+    static class Builder {
+        private final String projectCode
+        private final String projectDescription
+        private final List<Sample> samples
+        private float progress = 0
+        private FailedSamplesRatio failedSamples
+        private Date lastUpdate = new Date()
+
+        Builder(String projectCode, String projectDescription, List<Sample> samples) {
+            //todo verify the projectCode??
+            this.projectCode = projectCode
+            this.projectDescription = projectDescription
+            this.samples = samples
+
+            failedSamples = new FailedSamplesRatio(0,samples.size())
+        }
+
+        Builder progress(float value) {
+            //todo check that values are between 1-10
+            progress = value
+            return this
+        }
+
+        Builder failedSamples(int value) {
+            failedSamples = new FailedSamplesRatio(value,samples.size())
+            return this
+        }
+
+        Project build() {
+            return new Project(this)
+        }
+    }
+
+    private Project(Builder builder) {
+        projectCode = builder.projectCode
+        projectDescription = builder.projectDescription
+        samples = builder.samples
+        projectProgress = builder.progress
+        failedSamples = builder.failedSamples
+        lastUpdate = builder.lastUpdate
+    }
+}
