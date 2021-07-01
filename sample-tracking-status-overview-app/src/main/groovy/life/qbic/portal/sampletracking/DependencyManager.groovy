84a488f9ab6fcae2cbd148dcb3979a311454ad57
jnnfr <jennifer.boedker@qbic.uni-tuebingen.de>
2021-07-01 10:17:38 +0200

    Add dependency manager (#17)
    adds the dependency manager to the app

---
+++ sample-tracking-status-overview-app/src/main/groovy/life/qbic/portal/sampletracking/DependencyManager.groovy
@@ -0,0 +1,13 @@
+package life.qbic.portal.sampletracking
+/**
+ * <h1>Class that manages all the dependency injections and class instance creations</h1>
+ *
+ * <p>This class has access to all classes that are instantiated at setup. It is responsible to construct
+ * and provide every instance with it's dependencies injected. The class should only be accessed once upon
+ * portlet creation and shall not be used later on in the control flow.</p>
+ *
+ * @since 1.0.0
+ *
+*/
+class DependencyManager {
+}
