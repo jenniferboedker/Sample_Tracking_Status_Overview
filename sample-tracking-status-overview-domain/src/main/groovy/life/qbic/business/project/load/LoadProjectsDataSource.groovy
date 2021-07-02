db7adc6df3a44afc9b7550432b186625999c0509
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2021-07-02 11:39:29 +0200

    Add interface method

--- sample-tracking-status-overview-domain/src/main/groovy/life/qbic/business/project/load/LoadProjectsDataSource.groovy
+++ sample-tracking-status-overview-domain/src/main/groovy/life/qbic/business/project/load/LoadProjectsDataSource.groovy
@@ -11,13 +11,11 @@ import life.qbic.datamodel.dtos.projectmanagement.ProjectIdentifier
  */
 interface LoadProjectsDataSource {
 
-    //TODO implement
-    // suggestion:
-//    /**
-//     * Loads project identifiers given a user identifier
-//     * @param userId the identifier that can be used to retrieve projects
-//     * @return a list of project identifiers the user has access to
-//     * @since 1.0.0
-//     */
-//    List<ProjectIdentifier> projectIdentifiersForUser(String userId)
+    /**
+     * Loads project identifiers given a user identifier
+     * @param userId the identifier that can be used to retrieve projects
+     * @return a list of project identifiers the user has access to
+     * @since 1.0.0
+     */
+    List<ProjectIdentifier> projectIdentifiersForUser(String userId)
 }
