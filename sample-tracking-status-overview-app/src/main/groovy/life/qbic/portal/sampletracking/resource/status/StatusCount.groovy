06fe6794a92ae1fced20cfb476c35c73a8b1a2eb
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2021-07-07 14:37:35 +0200

    Add resource service for status count updates
    Signed-off-by: Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>

---
+++ sample-tracking-status-overview-app/src/main/groovy/life/qbic/portal/sampletracking/resource/status/StatusCount.groovy
@@ -0,0 +1,21 @@
+package life.qbic.portal.sampletracking.resource.status
+
+import life.qbic.datamodel.samples.Status
+
+/**
+ * <p>A DTO representing a status count for a specific project</p>
+ *
+ * @since 1.0.0
+ */
+class StatusCount {
+
+    final String projectId
+    final Status status
+    final int count
+
+    StatusCount(String projectId, Status status, int count) {
+        this.projectId = Objects.requireNonNull(projectId, "The project id must not be null.")
+        this.status = Objects.requireNonNull(status, "The sample status must not be null.")
+        this.count = Objects.requireNonNull(count, "The count must not be null.")
+    }
+}
---
+++ sample-tracking-status-overview-app/src/main/groovy/life/qbic/portal/sampletracking/resource/status/StatusCountResourceService.groovy
@@ -0,0 +1,44 @@
+package life.qbic.portal.sampletracking.resource.status
+
+import life.qbic.datamodel.samples.Status
+import life.qbic.portal.sampletracking.communication.Topic
+import life.qbic.portal.sampletracking.resource.ResourceService
+
+/**
+ * <b>A resource service holding data on status counts per project</b>
+ *
+ * <p>This service offers functionality to publish status counts for a specific project.</p>
+ *
+ * @since 1.0.0
+ */
+class StatusCountResourceService extends ResourceService<StatusCount>{
+
+    StatusCountResourceService() {
+        this.addTopic(Topic.SAMPLE_RECEIVED_COUNT_UPDATE)
+    }
+
+    @Override
+    void addToResource(StatusCount statusCount) {
+        switch (statusCount.status) {
+            case Status.SAMPLE_RECEIVED:
+                publish(statusCount, Topic.SAMPLE_RECEIVED_COUNT_UPDATE)
+                break
+            default:
+                // this is an unknown status. nothing is to be done
+                break
+        }
+
+    }
+
+    @Override
+    void removeFromResource(StatusCount statusCount) {
+        switch (statusCount.status) {
+            case Status.SAMPLE_RECEIVED:
+                publish(statusCount, Topic.SAMPLE_RECEIVED_COUNT_UPDATE)
+                break
+            default:
+                // this is an unknown status. nothing is to be done
+                break
+        }
+    }
+}
