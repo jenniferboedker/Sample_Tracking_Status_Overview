ca89a14527b8cda335cd151f1b75dccaaab8fd7a
wow-such-code <andreas.friedrich@uni-tuebingen.de>
2021-07-08 15:27:02 +0200

    return statuses instead of strings (#37)
    * return statuses instead of strings
    
    * update changelog

--- sample-tracking-status-overview-domain/src/main/groovy/life/qbic/business/samples/count/CountSamplesDataSource.groovy
+++ sample-tracking-status-overview-domain/src/main/groovy/life/qbic/business/samples/count/CountSamplesDataSource.groovy
@@ -1,5 +1,6 @@
 package life.qbic.business.samples.count
 
+import life.qbic.datamodel.samples.Status
 import life.qbic.business.DataSourceException
 
 
@@ -13,18 +14,11 @@ import life.qbic.business.DataSourceException
 interface CountSamplesDataSource {
 
     /**
-     * Counts samples of a project that have at least reached the status "sample received"
-     * @return number of samples that are part of this project and which have been received
+     * Returns all current sample statuses of a project
+     * @return List of Statuses for samples of a project
      * @since 1.0.0
      * @throws DataSourceException in case of a technical error with the data source
      */
-    List<String> countReceivedSamples(String projectCode) throws DataSourceException
-    
-    /**
-     * Counts all samples of a project
-     * @return number of samples that are part of this project
-     * @since 1.0.0
-     * @throws DataSourceException in case of a technical error with the data source
-     */
-    List<String> countSamples(String projectCode) throws DataSourceException
+    List<Status> fetchSampleStatusesForProject(String projectCode) throws DataSourceException
+
 }
