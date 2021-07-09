31d8040497900959a5d93894b99351f78ea12fe8
wow-such-code <andreas.friedrich@uni-tuebingen.de>
2021-07-09 15:50:47 +0200

    add more tests

--- sample-tracking-status-overview-domain/src/test/groovy/life/qbic/business/samples/count/CountSamplesSpec.groovy
+++ sample-tracking-status-overview-domain/src/test/groovy/life/qbic/business/samples/count/CountSamplesSpec.groovy
@@ -19,12 +19,54 @@ class CountSamplesSpec extends Specification {
         CountSamplesOutput output = Mock()
         CountSamples countSamples = new CountSamples(dataSource, output)
         when:"the use case is run"
-        countSamples.countSamples("QABCD")
+        countSamples.countReceivedSamples("QABCD")
         then:"a successful message is send"
-        1 * output.countedSamples(_ as Integer, _ as Integer)
+        1 * output.countedReceivedSamples(_ as Integer, _ as Integer)
         0 * output.failedExecution(_ as String)
     }
-
+    
+    def "amount of samples having reached received status is correctly returned"() {
+        given:
+        CountSamplesDataSource dataSource = Stub()
+        List<Status> statuses = [Status.SAMPLE_RECEIVED, Status.METADATA_REGISTERED, Status.SAMPLE_RECEIVED]
+        dataSource.fetchSampleStatusesForProject("QABCD") >> { statuses }
+        CountSamplesOutput output = Mock()
+        CountSamples countSamples = new CountSamples(dataSource, output)
+        when:"the use case is run"
+        countSamples.countReceivedSamples("QABCD")
+        then:"the correct amounts of samples are returned"
+        1 * output.countedReceivedSamples(3, 2)
+        0 * output.failedExecution(_ as String)
+    }
+    
+    def "amount of samples having reached AT LEAST received status is correctly returned"() {
+        given:
+        CountSamplesDataSource dataSource = Stub()
+        List<Status> statuses = [Status.DATA_AVAILABLE, Status.SAMPLE_RECEIVED, Status.METADATA_REGISTERED, Status.SAMPLE_RECEIVED]
+        dataSource.fetchSampleStatusesForProject("QABCD") >> { statuses }
+        CountSamplesOutput output = Mock()
+        CountSamples countSamples = new CountSamples(dataSource, output)
+        when:"the use case is run"
+        countSamples.countReceivedSamples("QABCD")
+        then:"the correct amounts of samples are returned"
+        1 * output.countedReceivedSamples(4, 3)
+        0 * output.failedExecution(_ as String)
+    }
+    
+    def "amount of samples having reached AT LEAST received status is correctly returned and deprecated statuses are not counted towards received samples"() {
+        given:
+        CountSamplesDataSource dataSource = Stub()
+        List<Status> statuses = [Status.DATA_AT_QBIC, Status.SAMPLE_RECEIVED, Status.METADATA_REGISTERED, Status.SAMPLE_RECEIVED]
+        dataSource.fetchSampleStatusesForProject("QABCD") >> { statuses }
+        CountSamplesOutput output = Mock()
+        CountSamples countSamples = new CountSamples(dataSource, output)
+        when:"the use case is run"
+        countSamples.countReceivedSamples("QABCD")
+        then:"the correct amounts of samples are returned"
+        1 * output.countedReceivedSamples(4, 2)
+        0 * output.failedExecution(_ as String)
+    }
+    
     def "unsuccessful execution of the use case lead to failure notifications"() {
         given:
         CountSamplesDataSource dataSource = Stub()
@@ -34,10 +76,10 @@ class CountSamplesSpec extends Specification {
         CountSamplesOutput output = Mock()
         CountSamples countSamples = new CountSamples(dataSource, output)
         when:"the use case is run"
-        countSamples.countSamples("QABCD")
+        countSamples.countReceivedSamples("QABCD")
         then:"a failure message is send"
         1 * output.failedExecution(_)
-        0 * output.countedSamples(_)
+        0 * output.countedReceivedSamples(_)
     }
 
     def "a DataSourceException leads to a failure notification and no samples being loaded"() {
@@ -50,10 +92,10 @@ class CountSamplesSpec extends Specification {
         CountSamples countSamples = new CountSamples(dataSource, output)
 
         when:"the use case is run"
-        countSamples.countSamples("QABCD")
+        countSamples.countReceivedSamples("QABCD")
 
         then:"a failure message is send"
         1 * output.failedExecution(_)
-        0 * output.countedSamples(_)
+        0 * output.countedReceivedSamples(_)
     }
 }
