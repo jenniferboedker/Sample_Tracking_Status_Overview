6c70744577d694e3c0c242c10561311526d44010
wow-such-code <andreas.friedrich@uni-tuebingen.de>
2021-08-23 16:19:38 +0200

    add download use case and tests (#57)
    * add download use case and tests
    
    Co-authored-by: Tobias Koch <KochTobi@users.noreply.github.com>

---
+++ sample-tracking-status-overview-domain/src/main/groovy/life/qbic/business/samples/download/DownloadSamplesOutput.groovy
@@ -0,0 +1,27 @@
+package life.qbic.business.samples.download
+
+/**
+ * <b>Output interface for the {@link DownloadSamples} feature</b>
+ *
+ * <p>Returns results from finding sample codes with downloadable data for one project</p>
+ *
+ * @since 1.0.0
+ */
+interface DownloadSamplesOutput {
+
+    /**
+     * To be called when the execution of the fetching sample codes failed
+     * @param reason the reason why the samples retrieval failed
+     * @since 1.0.0
+     */
+    void failedExecution(String reason)
+
+    /**
+     * To be called after successfully fetching sample codes with data for the provided code.
+     * @param projectCode the code of the project samples were counted for
+     * @param sampleCodes list of sample codes with available data
+     * @since 1.0.0
+     */
+    void foundDownloadableSamples(String projectCode, List<String> sampleCodes)
+
+}
