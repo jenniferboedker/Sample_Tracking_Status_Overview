8380bc312c76180b9afba0333a702dfbb04f4248
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2021-08-24 15:44:04 +0200

    add classes

---
+++ sample-tracking-status-overview-app/src/main/groovy/life/qbic/portal/sampletracking/components/projectoverview/download/DownloadProjectController.groovy
@@ -0,0 +1,19 @@
+package life.qbic.portal.sampletracking.components.projectoverview.download
+
+/**
+ * <b><short description></b>
+ *
+ * <p><detailed description></p>
+ *
+ * @since <version tag>
+ */
+class DownloadProjectController {
+
+    /**
+     * Triggers the download use case
+     * @param projectCode
+     */
+    void downloadProject(String projectCode) {
+        //TODO implement
+    }
+}
---
+++ sample-tracking-status-overview-app/src/main/groovy/life/qbic/portal/sampletracking/components/projectoverview/download/ManifestPresenter.groovy
@@ -0,0 +1,23 @@
+package life.qbic.portal.sampletracking.components.projectoverview.download
+
+import life.qbic.business.samples.download.ComposeManifestOutput
+
+/**
+ * <b><short description></b>
+ *
+ * <p><detailed description></p>
+ *
+ * @since <version tag>
+ */
+class ManifestPresenter implements ComposeManifestOutput {
+
+    @Override
+    void readManifest(String printedManifest) {
+        //TODO implement
+    }
+
+    @Override
+    void failedExecution(String reason) {
+        //TODO implement
+    }
+}
