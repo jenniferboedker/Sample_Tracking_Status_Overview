40f574c49b8188e8616c52f0d0911b7b2a588486
wow-such-code <andreas.friedrich@uni-tuebingen.de>
2021-08-26 16:50:04 +0200

    fix description
    Co-authored-by: jnnfr <jennifer.boedker@qbic.uni-tuebingen.de>

--- sample-tracking-status-overview-domain/src/main/groovy/life/qbic/business/samples/info/GetSamplesInfoDataSource.groovy
+++ sample-tracking-status-overview-domain/src/main/groovy/life/qbic/business/samples/info/GetSamplesInfoDataSource.groovy
@@ -13,7 +13,7 @@ import life.qbic.datamodel.samples.Status
 interface GetSamplesInfoDataSource {
 
     /**
-     * Given a list of sample codes, returns a maping between sample codes and the samples' names
+     * Given a list of sample codes, returns a mapping between sample codes and the associated sample names
      * @param List sampleCodes the codes of a number of samples
      * @return Map with sample codes as keys and sample names as values
      * @since 1.0.0
