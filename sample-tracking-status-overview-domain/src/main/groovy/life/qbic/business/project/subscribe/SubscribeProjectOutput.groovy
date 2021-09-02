000004d07395b6398c29ca99089467cc7ff426cd
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2021-09-02 09:49:54 +0200

    Create empty domain classes

---
+++ sample-tracking-status-overview-domain/src/main/groovy/life/qbic/business/project/subscribe/SubscribeProjectOutput.groovy
@@ -0,0 +1,11 @@
+package life.qbic.business.project.subscribe
+
+/**
+ * <b><short description></b>
+ *
+ * <p><detailed description></p>
+ *
+ * @since <version tag>
+ */
+class SubscribeProjectOutput {
+}
