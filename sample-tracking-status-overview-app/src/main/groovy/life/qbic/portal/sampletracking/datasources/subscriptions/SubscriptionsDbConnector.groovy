af50c07c524b3548c5aac806f6516c5ff4cf175e
wow-such-code <andreas.friedrich@uni-tuebingen.de>
2021-09-02 15:46:00 +0200

    add implementation for subscription data source

--- sample-tracking-status-overview-app/src/main/groovy/life/qbic/portal/sampletracking/datasources/subscriptions/SubscriptionsDbConnector.groovy
+++ sample-tracking-status-overview-app/src/main/groovy/life/qbic/portal/sampletracking/datasources/subscriptions/SubscriptionsDbConnector.groovy
@@ -1,7 +1,16 @@
 package life.qbic.portal.sampletracking.datasources.subscriptions
 
+import groovy.util.logging.Log4j2
+
 import life.qbic.business.project.subscribe.Subscriber
 import life.qbic.business.project.subscribe.SubscriptionDataSource
+
+import life.qbic.business.DataSourceException
+import life.qbic.portal.sampletracking.datasources.database.ConnectionProvider
+
+import java.sql.Connection
+import java.sql.PreparedStatement
+import java.sql.ResultSet
 
 /**
  * <b><short description></b>
@@ -11,15 +20,99 @@ import life.qbic.business.project.subscribe.SubscriptionDataSource
  * @since <version tag>
  */
 class SubscriptionsDbConnector implements SubscriptionDataSource {
+  
+  private final ConnectionProvider connectionProvider
+  
+      /**
+       * Creates a database connector that will connect to the database using the provided connection
+       * provider.
+       * @param connectionProvider the connection provider providing the connections that are used to
+       * connect to the database
+       * @since 1.0.0
+       */
+      SubscriptionsDbConnector(ConnectionProvider connectionProvider) {
+          this.connectionProvider = connectionProvider
+      }
+
     /**
-     * Creates the subscriber in the data source and subscribes him to the project.
+     * Creates the subscriber in the data source, if they don't exist, and subscribes them to the project.
      * @param subscriber
      * @param projectCode
      * @since 1.1.0
      */
     @Override
     void subscribeToProject(Subscriber subscriber, String projectCode) {
-        //TODO implement
-        println "subscriber = $subscriber, projectCode = $projectCode"
+          try {
+            Connection connection = connectionProvider.connect()
+            connection.setAutoCommit(false)
+      
+            connection.withCloseable { it ->
+              try {
+                int subscriberId = addSubscriber(it, subscriber)
+                addSubscription(it, subscriberId, projectCode)
+                connection.commit()
+              } catch (Exception e) {
+                log.error(e.message)
+                log.error(e.stackTrace.join("\n"))
+                connection.rollback()
+                throw new DataSourceException("Subscription to {$projectCode} could not be created: {$subscriber}")
+              } finally {
+                connection.close()
+              }
+            }
+          } catch (Exception e) {
+            log.error(e)
+            log.error(e.stackTrace.join("\n"))
+            throw new DataSourceException("Subscription to {$projectCode} could not be created: {$subscriber}")
+          }
+    }
+    
+    private int addSubscriber(Connection connection, Subscriber subscriber) {
+          subscriberId = getSubscriberId(subscriber)
+          if(subscriberId <= 0) {
+            String query = "INSERT INTO subscriber (first_name, last_name, email) VALUES(?, ?, ?)"
+            
+            def statement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)
+            
+            statement.setString(1, subscriber.firstName)
+            statement.setString(2, subscriber.lastName)
+            statement.setString(3, subscriber.eMail)
+            statement.execute()
+            def keys = statement.getGeneratedKeys()
+            while (keys.next()) {
+              subscriberId = keys.getInt(1)
+            }
+          }
+          return subscriberId
+    }
+    
+    private void addSubscription(Connection connection, int subscriberId, String projectCode) {
+          String query = "INSERT INTO subscription (project_code, subscriber_id) VALUES(?, ?)"
+            
+          def statement = connection.prepareStatement(query)
+            
+          statement.setInt(1, subscriberId)
+          statement.setString(2, projectCode)
+          statement.execute()
+    }
+
+    private int getSubscriberId(Subscriber subscriber) {
+          String query = "SELECT id FROM subscriber WHERE first_name = ? AND last_name = ? AND email = ?"
+          Connection connection = connectionProvider.connect()
+
+          int personId = -1
+
+          connection.withCloseable {
+            def statement = connection.prepareStatement(query)
+            statement.setString(1, person.firstName)
+            statement.setString(2, person.lastName)
+            statement.setString(3, person.emailAddress)
+
+            ResultSet result = statement.executeQuery()
+            while (result.next()) {
+              personId = result.getInt(1)
+            }
+          }
+          return personId
     }
 }
