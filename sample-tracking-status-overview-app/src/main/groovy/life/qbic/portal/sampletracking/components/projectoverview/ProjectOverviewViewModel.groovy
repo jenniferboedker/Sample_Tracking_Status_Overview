58963dd1607f490d8b700092dc6fa2018d767290
wow-such-code <andreas.friedrich@uni-tuebingen.de>
2021-10-05 16:16:59 +0200

    Sort projects by update/add comparator (#124)
    * add comparator and tests
    
    * sort project summaries by last change time
    
    * fix: create Instant object correctly by using ofEpochSecond
    
    * correctly set last change Instant in the model
    
    Co-authored-by: jnnfr <jennifer.boedker@qbic.uni-tuebingen.de>

--- sample-tracking-status-overview-app/src/main/groovy/life/qbic/portal/sampletracking/components/projectoverview/ProjectOverviewViewModel.groovy
+++ sample-tracking-status-overview-app/src/main/groovy/life/qbic/portal/sampletracking/components/projectoverview/ProjectOverviewViewModel.groovy
@@ -1,11 +1,15 @@
 package life.qbic.portal.sampletracking.components.projectoverview
 
 import groovy.beans.Bindable
+import java.util.Collections
+
 import life.qbic.business.project.Project
 import life.qbic.business.project.subscribe.Subscriber
 import life.qbic.portal.sampletracking.communication.Topic
 import life.qbic.portal.sampletracking.resource.ResourceService
 import life.qbic.portal.sampletracking.resource.status.StatusCount
+import life.qbic.portal.sampletracking.components.projectoverview.LastChangedComparator
+import life.qbic.portal.sampletracking.components.projectoverview.LastChangedComparator.SortOrder
 
 /**
  * <h1>ViewModel for the {@link ProjectOverviewView}</h1>
@@ -38,6 +42,8 @@ class ProjectOverviewViewModel {
         projectResourceService.iterator().each { Project project ->
             addProject(project)
         }
+        Collections.sort(projectOverviews, new LastChangedComparator(SortOrder.DESCENDING))
+
         statusCountService.iterator().each { StatusCount statusCount ->
             updateSamplesReceived(statusCount)
             updateSamplesFailedQc(statusCount)
