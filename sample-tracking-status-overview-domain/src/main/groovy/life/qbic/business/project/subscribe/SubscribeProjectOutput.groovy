f74d1e8deb1a7b0c3de78bdf7eb99badfb8aaeb9
wow-such-code <andreas.friedrich@uni-tuebingen.de>
2021-10-14 16:34:56 +0200

    makes since
    Co-authored-by: Steffengreiner <Steffen.Greiner@gmx.de>

--- sample-tracking-status-overview-domain/src/main/groovy/life/qbic/business/project/subscribe/SubscribeProjectOutput.groovy
+++ sample-tracking-status-overview-domain/src/main/groovy/life/qbic/business/project/subscribe/SubscribeProjectOutput.groovy
@@ -35,7 +35,7 @@ interface SubscribeProjectOutput {
      * Unsubscription was not possible
      * @param subscriber the subscriber that was provided
      * @param projectCode the project the unsubscription was attempted on
-     * @since 1.1.0
+     * @since 1.0.0
      */
     void unsubscriptionFailed(Subscriber subscriber, String projectCode)
 }
