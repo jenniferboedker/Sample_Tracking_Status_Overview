0457f8a2b804dbcc0a97e290984adf5ac7a3a84c
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2021-11-15 09:55:36 +0100

    Make sample fields accessible and final

--- sample-tracking-status-overview-domain/src/main/groovy/life/qbic/business/samples/Sample.groovy
+++ sample-tracking-status-overview-domain/src/main/groovy/life/qbic/business/samples/Sample.groovy
@@ -1,5 +1,6 @@
 package life.qbic.business.samples
 
+import groovy.transform.EqualsAndHashCode
 import life.qbic.datamodel.identifiers.SampleCodeFunctions
 import life.qbic.datamodel.samples.Status
 
@@ -10,26 +11,34 @@ import life.qbic.datamodel.samples.Status
  *
  * @since 1.0.0
  */
+@EqualsAndHashCode
 class Sample {
     /**
      * Sample code uniquely defining a sample
      */
-    private String code
+    final String code
     /**
      * Sample name
      */
-    private String name
+    final String name
     /**
      * Status of the sample
      */
-    private Status status
+    final Status status
 
-    Sample(String code, String name, Status status){
-        if(SampleCodeFunctions.isQbicBarcode(code)){
-           this.code = code
-        }else{
-            throw new IllegalArgumentException("Sample code is not a QBiC barcode")
+    /**
+     * Constructs a new Sample. Throws an IllegalArgumentException in case the sample code is not valid
+     * @param code the code of the sample
+     * @param name the name of the sample
+     * @param status the status the sample is in
+     * @throws IllegalArgumentException in case the sample code is not valid
+     * @since 1.0.0
+     */
+    Sample(String code, String name, Status status) throws IllegalArgumentException {
+        if (!SampleCodeFunctions.isQbicBarcode(code)) {
+            throw new IllegalArgumentException("Sample code $code is not a QBiC barcode")
         }
+        this.code = code
         this.name = Objects.requireNonNull(name, "Sample name must not be null")
         this.status = Objects.requireNonNull(status, "Sample status must not be null")
     }
