b2d5e37de29f6ecf49fbd8d3325ee486fa1d64f1
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2021-11-15 16:47:23 +0100

    Improve JavaDoc

--- sample-tracking-status-overview-domain/src/main/groovy/life/qbic/business/samples/info/SampleStatusDataSource.groovy
+++ sample-tracking-status-overview-domain/src/main/groovy/life/qbic/business/samples/info/SampleStatusDataSource.groovy
@@ -1,5 +1,6 @@
 package life.qbic.business.samples.info
 
+import life.qbic.business.DataSourceException
 import life.qbic.datamodel.samples.Status
 
 /**
@@ -9,5 +10,13 @@ import life.qbic.datamodel.samples.Status
  * @since 1.0.0
  */
 interface SampleStatusDataSource {
-    Map<String, Status> fetchSampleStatusesFor(Collection<String> sampleCodes)
+
+    /**
+     * Retrieves sample statuses for a collection of provided sample codes
+     * @param sampleCodes the sample codes of samples that should be checked for their sample status
+     * @return a map containing statueses for every sample code as key
+     * @throws DataSourceException in case of a technical failure
+     * @since 1.0.0
+     */
+    Map<String, Status> fetchSampleStatusesFor(Collection<String> sampleCodes) throws DataSourceException
 }
