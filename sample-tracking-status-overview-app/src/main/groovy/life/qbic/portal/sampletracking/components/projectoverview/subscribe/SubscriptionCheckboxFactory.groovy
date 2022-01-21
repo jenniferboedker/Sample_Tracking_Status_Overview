57a25b8f029b2adfec983d5fee85ae444a9fff4f
jnnfr <jennifer.boedker@qbic.uni-tuebingen.de>
2022-01-21 14:25:39 +0100

    Apply suggestions from code review
    Co-authored-by: Tobias Koch <KochTobi@users.noreply.github.com>

--- sample-tracking-status-overview-app/src/main/groovy/life/qbic/portal/sampletracking/components/projectoverview/subscribe/SubscriptionCheckboxFactory.groovy
+++ sample-tracking-status-overview-app/src/main/groovy/life/qbic/portal/sampletracking/components/projectoverview/subscribe/SubscriptionCheckboxFactory.groovy
@@ -37,7 +37,6 @@ class SubscriptionCheckboxFactory {
      * @return A checkbox which is already preconfigured with listeners and bound to the subscribe use case
      */
     CheckBox getSubscriptionCheckbox(ProjectSummary project){
-        println(project.toString())
 
         if(projectCodeToCheckBox.containsKey(project.code)){
             return projectCodeToCheckBox.get(project.code)
